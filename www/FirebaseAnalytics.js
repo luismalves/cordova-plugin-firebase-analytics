e650563502b907a67577d1c11fd7c3ad9fc70393
Luís Alves <luisalvesw@gmail.com>
2025-01-21 15:32:07 +0000

    Disable logECommerceEvent

--- www/FirebaseAnalytics.js
+++ www/FirebaseAnalytics.js
@@ -51,11 +51,11 @@ module.exports = {
     },
     requestTrackingAuthorization: function(showInformation, title, message, buttonTitle, success, error) {
         exec(success, error, PLUGIN_NAME, 'requestTrackingAuthorization', [showInformation, title, message, buttonTitle]);
-    },
+    },/*
     logECommerceEvent: function(event, eventParameters, items, success, error) {
         let args = [{event, eventParameters, items}];
         exec(success, error, PLUGIN_NAME, 'logECommerceEvent', args);
-    },
+    },*/
     /**
      * setConsent
      *
